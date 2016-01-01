4f60430d710e8dac65718ac517bd11256ab0a696
JunYoungJJang <kimjun136@naver.com>
2016-01-01 16:06:41 +0900

    Create Account.cpp

---
+++ Account.cpp
@@ -0,0 +1,137 @@
+#include "Account.h"
+#include <iostream>
+#include <cstdlib>
+#include <string.h>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+Account::Account() {
+	name = new char[6];
+	strcpy(name, "admin");
+	
+	id = new char[12];
+	strcpy(id, "00000-00000");
+	
+	money = 10000000;
+}
+
+Account::Account(const char* _name)
+{
+	if(_name == NULL) {
+		cout << "이름을 입력해주세요!" << endl;
+		return;
+	} else if(strlen(_name) > 12) {
+		cout << "이름이 너무 길어요..." << endl;
+		return;
+	}
+	money = 0;
+
+	name = new char[strlen(_name) + 1];
+	strcpy(name, _name);
+	
+	id = new char[12];			
+	for(int i=0; i<11; i++) {
+		if(i == 5) {
+			id[i] = '-';
+			continue;
+		}
+		id[i] = rand()%10 + '0';
+	}
+	id[11] = '\0';
+
+	cout << "계좌가 생성되었습니다." << endl;
+	cout << "이     름: " << name << endl;
+	cout << "계좌번호: " << id << endl;
+	cout << "잔     액: " << money << endl;
+
+	sleep(5);
+	system("clear");		
+}
+
+Account::~Account()
+{
+	delete []name;
+	delete []id;	
+}
+
+void Account::Deposit(unsigned int _money)
+{
+	money += _money;
+
+	cout << "\n거래가 성공적으로 이루어졌습니다." << endl;
+	cout << "잔    액: " << money << endl;
+
+	sleep(3);
+	system("clear");	
+}
+
+void Account::Withdraw(unsigned int _money)
+{
+	if(_money > money) {
+		cout << "\n잔액을 확인해주세요." << endl;
+		cout << "잔    액: " << money<< endl;
+
+		sleep(3);
+		system("clear");
+	} else {
+		money -= _money;
+		
+		cout << "\n거래가 성공적으로 이루어졌습니다." << endl;
+		cout << "잔    액: " << money << endl;
+	
+		sleep(3);
+		system("clear");
+	}
+}
+
+// 수정: reference 식 참조
+int Account::identify(Account* arr[])
+{
+	int i;
+	char _name[20], _id[12];
+
+	cout << "본인 확인을 위해 아래 정보를 입력해주세요." << endl;
+	cout << "이    름: ";	
+	cin >> _name;
+
+	cout << "계좌번호: ";
+	cin >> _id;
+	
+	for(i=0; arr[i]; i++) {
+		if(!strcmp(arr[i]->name, _name) && !strcmp(arr[i]->id, _id)) {
+			return i;
+		}
+	}
+
+	return -1;	
+}
+
+void Account::ShowInfo()
+{
+	char id[12];
+
+	strcpy(id, this->id);
+	for(int i=6; i<11; i++) {
+		id[i] = '*';
+	}
+
+	cout << "이    름: " << name << endl;
+	cout << "계좌번호: " << id << endl;
+	cout << "잔    액: " << money << endl;
+	cout << endl;
+}
+
+void Account::ShowMenu()
+{
+	cout << "=== 준영 은행입니다~~~ ===" << endl;
+	cout << "1. 계좌 개설" << endl;
+	cout << "2. 입금" << endl;
+	cout << "3. 출금" << endl;
+	cout << "4. 전체 고객 잔액 조회" << endl;
+	cout << "5. 종료 " << endl;
+
+	cout << "\n입력: ";
+}
+
