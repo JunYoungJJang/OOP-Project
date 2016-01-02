fb5ffed0c79cfc844024c6fdde4cf2ddf9b94548
JunYoungJJang <kimjun136@naver.com>
2016-01-02 18:29:13 +0900

    Update Account.h

--- Account.h
+++ Account.h
@@ -1,19 +1,19 @@
 class Account
 {
-	const char* name;
-	const char* id;
+	char* name;
+	char* id;
 	unsigned int money;
 public:
 	Account();
 	Account(const char* _name);
-	Account(const Account& temp);   // 복사 생성자
+	Account(const Account& temp);
 	~Account();
 
 	void Deposit(unsigned int _money);
 	void Withdraw(unsigned int _money);
 
-	static int identify(Account* arr[]);
+	int identify(Account* arr[]);
 
-	static void ShowInfo(Account* arr[]);
-	static void ShowMenu();	
+	void ShowInfo();
+	void ShowMenu();	
 };
