d80ab68e3555bffe35541607c95cefe4fe076486
JunYoungJJang <kimjun136@naver.com>
2016-01-22 22:43:41 +0900

    Create bank3.0.cpp

---
+++ bank3.0.cpp
@@ -0,0 +1,48 @@
+#include "AccManager.h"
+#include <iostream>
+
+unsigned int AccManager::cnt=0;
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main(void)
+{
+	AccManager hana;
+	int choice;
+
+	while(1) {
+		hana.menu();
+		cin >> choice;
+		if(choice < 0 || choice >5) {
+			cout << "잘못된 입력입니다." << endl<<endl;
+			continue;
+		}
+	
+		switch(choice) {
+		case 1:
+			hana.makeAccount();
+			break;
+		case 2:
+			hana.inOut(DEPOSIT);
+			break;
+		case 3:
+			hana.inOut(WITHDRAW);
+			break;
+		case 4:
+			hana.remit();
+			break;
+		case 5:
+			hana.showAllUsers();
+			break;
+		}
+
+		if(choice == 0) {
+			break;
+		}
+	}
+	cout << "이용해주셔서 감사합니다." << endl;
+
+	return 0;
+}
