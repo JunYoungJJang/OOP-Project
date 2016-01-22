b1c5482a5da1c75e35766a3b1cd321d34bb27dae
JunYoungJJang <kimjun136@naver.com>
2016-01-22 22:36:54 +0900

    Create AccManager.h

---
+++ AccManager.h
@@ -0,0 +1,25 @@
+#include "Account.h"
+
+#define MAX_USER	30
+
+typedef enum {
+	DEPOSIT=1, WITHDRAW
+} mode;
+
+class AccManager
+{
+private:
+	Account* user[MAX_USER];
+	static unsigned int cnt;
+public:
+	AccManager() { }
+	~AccManager();
+
+	void makeAccount();
+	void inOut(mode sel);   // 입출금
+	void remit(); 		// 송금
+	void showAllUsers();
+	void menu();
+	
+	bool identify(const Account* src, const Account& dest);
+};
