d424d23a953b201d83d96212419e3e3263b5dbe9
JunYoungJJang <kimjun136@naver.com>
2016-01-23 22:34:36 +0900

    Update Account.h

--- Account.h
+++ Account.h
@@ -8,13 +8,46 @@ private:
 public:
 	Account(const char* _name=NULL, const char* _id=NULL, const char* _pw=NULL);
 	Account(const Account& a);
-	~Account();
+	virtual ~Account();
 	
-	const char* GetName() const; 
+	const char* GetName() const;
 	const char* GetID() const;
 	const char* GetPW() const;
 	unsigned int GetBal() const;
 	
-	void AddMoney(int money);
-	void MinMoney(int money);
+	virtual void AddMoney(unsigned int money);
+	void MinMoney(unsigned int money);
+
+	virtual void ShowAll() const;
 };
+
+class CreditAcc : public Account
+{
+private:
+	unsigned int interest;
+public:
+	CreditAcc(const char* _name=NULL, const char* _id=NULL, const char* _pw=NULL);
+	CreditAcc(const CreditAcc& c);
+	virtual ~CreditAcc();
+	
+	unsigned int GetInterest() const;
+	virtual void AddMoney(unsigned int money);	
+
+	virtual void ShowAll() const;
+};
+
+class DonationAcc : public Account
+{
+private:
+	unsigned int donation;
+public:
+	DonationAcc(const char* _name=NULL, const char* _id=NULL, const char* _pw=NULL);
+	DonationAcc(const DonationAcc& d);
+	virtual ~DonationAcc();
+
+	unsigned int GetDonation() const;
+	virtual void AddMoney(unsigned int money);
+
+	virtual void ShowAll() const;
+};
+
